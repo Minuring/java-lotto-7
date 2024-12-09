713d4ae2ae16dbafd3a57de2ddf2c5f3dd4ebf38
Minuring <mw9505@gmail.com>
2024-12-09 23:15:48 +0900

    feat: 당첨 정보 입력
    당첨 번호와 보너스번호를 입력하는 기능입니다.

--- src/main/java/lotto/presentation/InputView.java
+++ src/main/java/lotto/presentation/InputView.java
@@ -1,20 +1,35 @@
 package lotto.presentation;
 
 import camp.nextstep.edu.missionutils.Console;
-import lotto.error.exception.InvalidBuyAmountException;
+import java.util.Arrays;
+import java.util.List;
+import lotto.domain.Lotto;
 
 public class InputView {
 
     public static long readBuyAmount() {
         System.out.println("구입금액을 입력해 주세요.");
-        return toLong(Console.readLine());
+        return Long.parseLong(Console.readLine());
     }
 
-    private static long toLong(String input) {
-        try {
-            return Long.parseLong(input);
-        } catch (NumberFormatException e) {
-            throw new InvalidBuyAmountException();
-        }
+    public static Lotto readWinningLotto() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String input = Console.readLine();
+
+        List<Integer> numbers = toListByDelimiter(input, ",");
+        return new Lotto(numbers);
+    }
+
+    public static int readBonusNumber() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+        String input = Console.readLine();
+        return Integer.parseInt(input);
+    }
+
+    private static List<Integer> toListByDelimiter(String input, String delimiter) {
+        String[] rawNumbers = input.split(delimiter);
+        return Arrays.stream(rawNumbers)
+            .map(Integer::parseInt)
+            .toList();
     }
 }
--- src/test/java/lotto/presentation/InputViewTest.java
+++ src/test/java/lotto/presentation/InputViewTest.java
@@ -2,17 +2,35 @@ package lotto.presentation;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
+import lotto.domain.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class InputViewTest extends IOTest {
 
-    @DisplayName("입력 요구 메세지 출력")
+    @DisplayName("로또 구입금액 입력 요구 메세지 출력")
     @Test
-    void displayHeader() {
+    void displayBuyAmountHeader() {
         systemIn("1000"); //입력 무한대기 방지를 위한 dummy 값
         InputView.readBuyAmount();
         assertThat(systemOutput()).contains("구입금액을 입력해 주세요.");
+    }
+
+    @DisplayName("당첨번호 입력 요구 메세지 출력")
+    @Test
+    void displayWinningNumbersHeader() {
+        systemIn("1,2,3,4,5,6"); //입력 무한대기 방지를 위한 dummy 값
+        InputView.readWinningLotto();
+        assertThat(systemOutput()).contains("당첨 번호를 입력해 주세요.");
+    }
+
+    @DisplayName("보너스 번호 입력 요구 메세지 출력")
+    @Test
+    void displayBonusNumbersHeader() {
+        systemIn("7"); //입력 무한대기 방지를 위한 dummy 값
+        InputView.readBonusNumber();
+        assertThat(systemOutput()).contains("보너스 번호를 입력해 주세요.");
     }
 
     @DisplayName("로또 구입금액 읽기")
@@ -21,5 +39,22 @@ class InputViewTest extends IOTest {
         systemIn("2000");
         long buyAmount = InputView.readBuyAmount();
         assertThat(buyAmount).isEqualTo(2000L);
+    }
+
+    @DisplayName("로또 당첨번호 읽기")
+    @Test
+    void readWinningLotto() {
+        systemIn("1,2,3,4,5,6");
+        Lotto winningLotto = InputView.readWinningLotto();
+        assertThat(winningLotto.getNumbers())
+            .containsExactly(1, 2, 3, 4, 5, 6);
+    }
+
+    @DisplayName("로또 보너스 번호 읽기")
+    @Test
+    void readBonusNumber() {
+        systemIn("7");
+        int bonusNumber = InputView.readBonusNumber();
+        assertThat(bonusNumber).isEqualTo(7);
     }
 }
